84e6de3e3f46dce4992a2da7a7851964e96fab23
Edoardo Rosa <edoardo.rosa90@gmail.com>
2017-11-07 10:13:19 +0100

    test set for PEG

---
+++ src/com/mlisa/gomath/tests/peg_test.go
@@ -0,0 +1,35 @@
+package pegmatch_test
+
+import (
+	"bytes"
+	"com/mlisa/gomath/parser"
+	"fmt"
+	"testing"
+)
+
+func TestPegmatchSimple(t *testing.T) {
+	operation := "4+2"
+	got, err := parser.ParseReader("", bytes.NewBufferString(operation))
+	if err != nil || got.(int) != 6 {
+		t.Error("failed")
+	}
+	fmt.Println(got)
+}
+
+func TestPegmatchMedium(t *testing.T) {
+	operation := "3*(4+2)"
+	got, err := parser.ParseReader("", bytes.NewBufferString(operation))
+	if err != nil || got.(int) != 18 {
+		t.Error("failed")
+	}
+	fmt.Println(got)
+}
+
+func TestPegmatchHard(t *testing.T) {
+	operation := "3*(4+2)/(2+4*4)+1"
+	got, err := parser.ParseReader("", bytes.NewBufferString(operation))
+	if err != nil || got.(int) != 2 {
+		t.Error("failed")
+	}
+	fmt.Println(got)
+}
