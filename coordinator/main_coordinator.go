365f039f532c6b3379b702f22bf24bac0ca7e88e
Edoardo Rosa <edoardo.rosa90@gmail.com>
2017-11-07 10:54:08 +0100

    fix import repo refactoring

--- coordinator/main_coordinator.go
+++ coordinator/main_coordinator.go
@@ -4,8 +4,6 @@ import (
 	"log"
 	"runtime"
 
-	"github.com/mlisa/gomath/coordinator"
-
 	console "github.com/AsynkronIT/goconsole"
 	"github.com/AsynkronIT/protoactor-go/actor"
 	"github.com/AsynkronIT/protoactor-go/remote"
@@ -17,7 +15,7 @@ func main() {
 	address := "127.0.0.1:8081"
 	remote.Start(address)
 
-	props := actor.FromInstance(&coordinator.Coordinator{MaxPeers: 50, Peers: make([]*actor.PID, 0, 50)})
+	props := actor.FromInstance(&Coordinator{MaxPeers: 50, Peers: make([]*actor.PID, 0, 50)})
 	_, err := actor.SpawnNamed(props, id)
 	if err != nil {
 		log.Panicln(err)
