07f163a62d2f0b64dc80b9190b472f824dbf1d64
Edoardo Rosa <edoardo.rosa90@gmail.com>
2017-11-08 11:16:22 +0100

    read config from file

--- common/stuff.go
+++ common/stuff.go
@@ -19,6 +19,20 @@ type Config struct {
 	Coordinators []*actor.PID
 }
 
+func GetFileConfig(path string) Config {
+	absPath, _ := filepath.Abs(filepath.Clean(path))
+	configuration := Config{}
+	file, err := os.Open(absPath)
+	if err != nil {
+		log.Println("[ERROR] " + err.Error())
+	}
+	defer file.Close()
+	if err = json.NewDecoder(file).Decode(&configuration); err != nil {
+		log.Fatalln("[ERROR] " + err.Error())
+	}
+	return configuration
+}
+
 func GetConfig(who string) Config {
 	fileName := "config_" + who + ".json"
 	absPath, _ := filepath.Abs(filepath.Clean(fileName))
