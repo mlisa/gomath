c454c1243adfc200990de94d52ef17e8bbf18a73
Edoardo Rosa <edoardo.rosa90@gmail.com>
2017-11-27 19:20:48 +0100

    fix PEG tests

--- tests/peg_test.go
+++ tests/peg_test.go
@@ -2,7 +2,6 @@ package pegmatch_test
 
 import (
 	"bytes"
-	"fmt"
 	"log"
 	"testing"
 	"time"
@@ -14,33 +13,37 @@ func TestPegmatchSimple(t *testing.T) {
 	operation := "4+2"
 	start := time.Now()
 	got, err := parser.ParseReader("", bytes.NewBufferString(operation))
-	log.Printf("STRING: %s", operation)
 	if err != nil || got.(int) != 6 {
 		t.Error("failed")
 	}
-	log.Printf("Time %s", time.Since(start))
+	log.Printf("STRING: %s\n", operation)
+	log.Printf("RESULT: %d\n", got.(int))
+	log.Printf("TIME %s\n", time.Since(start))
+	log.Println("---------------------------------------------------------------------------------")
 }
 
 func TestPegmatchMedium(t *testing.T) {
 	operation := "3*(4+2)"
 	start := time.Now()
 	got, err := parser.ParseReader("", bytes.NewBufferString(operation))
-	log.Printf("STRING: %s", operation)
 	if err != nil || got.(int) != 18 {
 		t.Error("failed")
 	}
-	log.Printf("Time %s", time.Since(start))
-	fmt.Println(got)
+	log.Printf("STRING: %s\n", operation)
+	log.Printf("RESULT: %d\n", got.(int))
+	log.Printf("Time %s\n", time.Since(start))
+	log.Println("---------------------------------------------------------------------------------")
 }
 
 func TestPegmatchHard(t *testing.T) {
 	operation := "3*(4+2)/(2+4*4)+1"
 	start := time.Now()
 	got, err := parser.ParseReader("", bytes.NewBufferString(operation))
-	log.Printf("STRING: %s", operation)
 	if err != nil || got.(int) != 2 {
 		t.Error("failed")
 	}
-	log.Printf("Time %s", time.Since(start))
-	fmt.Println(got)
+	log.Printf("STRING: %s\n", operation)
+	log.Printf("RESULT: %d\n", got.(int))
+	log.Printf("Time %s\n", time.Since(start))
+	log.Println("---------------------------------------------------------------------------------")
 }
