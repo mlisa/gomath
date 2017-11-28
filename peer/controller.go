8ebf8cc1c7a6fe14edfd4588057f3418a2fdb8bb
lisamazzini <lisa.mazzini5@gmail.com>
2017-11-28 16:55:33 +0100

    Added log

--- peer/controller.go
+++ peer/controller.go
@@ -49,6 +49,7 @@ func (controller *Controller) AskForResult(operation string) {
 		if _, err := parser.Parse("", []byte(operation)); err == nil {
 			if resultInLocalCache := controller.SearchInCache(operation); resultInLocalCache != "" {
 				controller.SetOutput(resultInLocalCache)
+				controller.Log(OFFLINECOMPUTATION, "")
 			} else {
 				var complexity = int64(strings.Count(operation, "*")*2 + strings.Count(operation, "/")*2 +
 					strings.Count(operation, "+") + strings.Count(operation, "-"))
